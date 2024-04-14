ef91d06d537b4d0152257c38f8d338899a9c676f
Chirayu Rana <chirayurana@yahoo.com>
2024-04-14 11:20:17 +0545

    Delete app directory
    initial Commit

--- app/src/main/java/com/chirayu/financeapp/screens/Reports.kt
+++
@@ -1,9 +0,0 @@
-package com.chirayu.financeapp.screens
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun Reports(navController: NavController){
-
-}
